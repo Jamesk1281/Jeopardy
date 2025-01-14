b77a9dded772f727bd57b299711626f48c6adfad
Jamesk1281 <26jkouvlis@xbhs.net>
2025-01-13 20:17:49 -0500

    Initial Commit

---
+++ Jeopardy/JeopardyApp.swift
@@ -0,0 +1,17 @@
+//
+//  JeopardyApp.swift
+//  Jeopardy
+//
+//  Created by James . on 1/13/25.
+//
+
+import SwiftUI
+
+@main
+struct JeopardyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
